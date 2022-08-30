7e2c89e1172f97fdafdc6e5bd245b357f0577c7c
Andres2310 <and.23108@gmail.com>
2022-08-30 11:29:57 -0400

    improved design

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,6 +1,6 @@
 import ImageBox from "./ImageBox"
 import SearchBox from "./SearchBar"
-import { useState, useEffect,useRef } from "react"
+import { useState, useEffect, useRef } from "react"
 
 import getPokemonByName from "../services/getData"
 import HistoryButton from "./HistoryButton"
@@ -13,9 +13,11 @@ function Container() {
   const [nameSearch, setNameSearch] = useState("")
   const [searchHistory, setSearchHistory] = useState([])
   useEffect(() => {
+
     getPokemonByName(nameSearch)
       .then((foundPokemon) => {
         setPokemon(foundPokemon)
+        console.log(foundPokemon);
         setSearchHistory([...searchHistory, foundPokemon.sprites.front_default])
       })
       .catch(err => console.error(err))
@@ -27,13 +29,13 @@ function Container() {
       {
         pokemon ? <ImageBox pokemon={pokemon} /> : <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
           <strong class="font-bold">Pokemon no encontrado!</strong>
-          <span class="block sm:inline">Busque nuevamente.</span>
+          <span class="block sm:inline ml-2">Busque nuevamente.</span>
         </div>
       }
 
-      <HistoryButton show={() => modalRef.current.showModal()}/>
-      <ModalHistory modalRef={modalRef} searchHistory={searchHistory}/>
-     
+      <HistoryButton show={() => modalRef.current.showModal()} />
+      <ModalHistory modalRef={modalRef} searchHistory={searchHistory} />
+
     </main>
   )
 }
--- src/components/ImageBox.jsx
+++ src/components/ImageBox.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import Toggle from "./Toggle"
+import defaultImg from "../assets/poke-shadow.png"
 
 function ImageBox({ pokemon }) {
 
@@ -9,10 +10,14 @@ function ImageBox({ pokemon }) {
 
   const [isShinny, setIsShinny] = useState(false)
   return (
-    <div className="flex flex-col items-center gap-3">
-      <h1 className="text-lg text-slate-300 font-semibold">{pokemon.name}</h1>
-      <img className="w-60 max-w-md drop-shadow-xl border-1" src={isShinny ? pokemon?.sprites?.front_shiny : pokemon?.sprites?.front_default} />
-      <Toggle handleChange={() => setIsShinny(!isShinny)} enabled={isShinny} />
+    <div className="flex flex-col items-center gap-3 shadow-md shadow-slate-600/70 p-4">
+      <div className="flex justify-between items-center w-full">
+        <Toggle handleChange={() => setIsShinny(!isShinny)} enabled={isShinny} />
+        <h1 className="capitalize text-2xl text-slate-300 font-semibold">{pokemon.name}</h1>
+      </div>
+      {
+        pokemon ? <img className="w-60 max-w-md drop-shadow-xl border-1" src={isShinny ? pokemon?.sprites?.front_shiny : pokemon?.sprites?.front_default} /> : <p>hola</p>
+      }
       <button onClick={handleClick} className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
         <svg className="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" /></svg>
         <span>Download</span>
